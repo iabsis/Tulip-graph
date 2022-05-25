6b35b11401960dd385ff935db2a8512576f000b2
Gilles Hemmerlé <giloux@gmail.com>
2022-05-25 11:57:21 +0200

    Debug

--- generated/schema.ts
+++ generated/schema.ts
@@ -11,7 +11,7 @@ import {
   BigDecimal
 } from "@graphprotocol/graph-ts";
 
-export class ExampleEntity extends Entity {
+export class TulipEntity extends Entity {
   constructor(id: string) {
     super();
     this.set("id", Value.fromString(id));
@@ -19,18 +19,18 @@ export class ExampleEntity extends Entity {
 
   save(): void {
     let id = this.get("id");
-    assert(id != null, "Cannot save ExampleEntity entity without an ID");
+    assert(id != null, "Cannot save TulipEntity entity without an ID");
     if (id) {
       assert(
         id.kind == ValueKind.STRING,
-        `Entities of type ExampleEntity must have an ID of type String but the id '${id.displayData()}' is of type ${id.displayKind()}`
+        `Entities of type TulipEntity must have an ID of type String but the id '${id.displayData()}' is of type ${id.displayKind()}`
       );
-      store.set("ExampleEntity", id.toString(), this);
+      store.set("TulipEntity", id.toString(), this);
     }
   }
 
-  static load(id: string): ExampleEntity | null {
-    return changetype<ExampleEntity | null>(store.get("ExampleEntity", id));
+  static load(id: string): TulipEntity | null {
+    return changetype<TulipEntity | null>(store.get("TulipEntity", id));
   }
 
   get id(): string {
@@ -40,15 +40,6 @@ export class ExampleEntity extends Entity {
 
   set id(value: string) {
     this.set("id", Value.fromString(value));
-  }
-
-  get count(): BigInt {
-    let value = this.get("count");
-    return value!.toBigInt();
-  }
-
-  set count(value: BigInt) {
-    this.set("count", Value.fromBigInt(value));
   }
 
   get owner(): Bytes {
@@ -67,5 +58,14 @@ export class ExampleEntity extends Entity {
 
   set approved(value: Bytes) {
     this.set("approved", Value.fromBytes(value));
+  }
+
+  get claimed(): boolean {
+    let value = this.get("claimed");
+    return value!.toBoolean();
+  }
+
+  set claimed(value: boolean) {
+    this.set("claimed", Value.fromBoolean(value));
   }
 }
--- src/tulip-art.ts
+++ src/tulip-art.ts
@@ -1,86 +1,96 @@
 import { BigInt } from "@graphprotocol/graph-ts"
 import {
-  TulipArt,
-  Approval,
-  ApprovalForAll,
-  BaseURIChanged,
-  ContollerUpdated,
-  OwnershipTransferred,
+
   Transfer
 } from "../generated/TulipArt/TulipArt"
-import { ExampleEntity } from "../generated/schema"
 
-export function handleApproval(event: Approval): void {
-  // Entities can be loaded from the store using a string ID; this ID
-  // needs to be unique across all entities of the same type
-  let entity = ExampleEntity.load(event.transaction.from.toHex())
+import { PrizePoolAwarded } from "../generated/TulipArtLottery/TulipArtLottery"
+import { TulipEntity } from "../generated/schema"
+import { log } from '@graphprotocol/graph-ts'
 
-  // Entities only exist after they have been saved to the store;
-  // `null` checks allow to create entities on demand
-  if (!entity) {
-    entity = new ExampleEntity(event.transaction.from.toHex())
+// export function handleApproval(event: Approval): void {
+//   // Entities can be loaded from the store using a string ID; this ID
+//   // needs to be unique across all entities of the same type
+//   let entity = TulipEntity.load(event.transaction.from.toHex())
 
-    // Entity fields can be set using simple assignments
-    entity.count = BigInt.fromI32(0)
-  }
+//   // Entities only exist after they have been saved to the store;
+//   // `null` checks allow to create entities on demand
+//   if (!entity) {
+//     entity = new TulipEntity(event.transaction.from.toHex())
 
-  // BigInt and BigDecimal math are supported
-  entity.count = entity.count + BigInt.fromI32(1)
+//     // Entity fields can be set using simple assignments
+//     entity.count = BigInt.fromI32(0)
+//   }
 
-  // Entity fields can be set based on event parameters
-  entity.owner = event.params.owner
-  entity.approved = event.params.approved
+//   // BigInt and BigDecimal math are supported
+//   entity.count = entity.count + BigInt.fromI32(1)
 
-  // Entities can be written to the store with `.save()`
-  entity.save()
+//   // Entity fields can be set based on event parameters
+//   entity.owner = event.params.owner
+//   entity.approved = event.params.approved
 
-  // Note: If a handler doesn't require existing field values, it is faster
-  // _not_ to load the entity from the store. Instead, create it fresh with
-  // `new Entity(...)`, set the fields that should be updated and save the
-  // entity back to the store. Fields that were not set or unset remain
-  // unchanged, allowing for partial updates to be applied.
+//   // Entities can be written to the store with `.save()`
+//   entity.save()
 
-  // It is also possible to access smart contracts from mappings. For
-  // example, the contract that has emitted the event can be connected to
-  // with:
-  //
-  // let contract = Contract.bind(event.address)
-  //
-  // The following functions can then be called on this contract to access
-  // state variables and other data:
-  //
-  // - contract.balanceOf(...)
-  // - contract.baseURI(...)
-  // - contract.controllers(...)
-  // - contract.getApproved(...)
-  // - contract.getTraitsForId(...)
-  // - contract.idsToTraits(...)
-  // - contract.isApprovedForAll(...)
-  // - contract.isController(...)
-  // - contract.lowerBound(...)
-  // - contract.name(...)
-  // - contract.numberOfTraits(...)
-  // - contract.owner(...)
-  // - contract.ownerOf(...)
-  // - contract.setTokenWinner(...)
-  // - contract.supportsInterface(...)
-  // - contract.symbol(...)
-  // - contract.toBeMinted(...)
-  // - contract.tokenByIndex(...)
-  // - contract.tokenOfOwnerByIndex(...)
-  // - contract.tokenURI(...)
-  // - contract.tokenWinner(...)
-  // - contract.tokensToBeMinted(...)
-  // - contract.totalSupply(...)
-  // - contract.upperBound(...)
+//   // Note: If a handler doesn't require existing field values, it is faster
+//   // _not_ to load the entity from the store. Instead, create it fresh with
+//   // `new Entity(...)`, set the fields that should be updated and save the
+//   // entity back to the store. Fields that were not set or unset remain
+//   // unchanged, allowing for partial updates to be applied.
+
+//   // It is also possible to access smart contracts from mappings. For
+//   // example, the contract that has emitted the event can be connected to
+//   // with:
+//   //
+//   // let contract = Contract.bind(event.address)
+//   //
+//   // The following functions can then be called on this contract to access
+//   // state variables and other data:
+//   //
+//   // - contract.balanceOf(...)
+//   // - contract.baseURI(...)
+//   // - contract.controllers(...)
+//   // - contract.getApproved(...)
+//   // - contract.getTraitsForId(...)
+//   // - contract.idsToTraits(...)
+//   // - contract.isApprovedForAll(...)
+//   // - contract.isController(...)
+//   // - contract.lowerBound(...)
+//   // - contract.name(...)
+//   // - contract.numberOfTraits(...)
+//   // - contract.owner(...)
+//   // - contract.ownerOf(...)
+//   // - contract.setTokenWinner(...)
+//   // - contract.supportsInterface(...)
+//   // - contract.symbol(...)
+//   // - contract.toBeMinted(...)
+//   // - contract.tokenByIndex(...)
+//   // - contract.tokenOfOwnerByIndex(...)
+//   // - contract.tokenURI(...)
+//   // - contract.tokenWinner(...)
+//   // - contract.tokensToBeMinted(...)
+//   // - contract.totalSupply(...)
+//   // - contract.upperBound(...)
+// }
+
+
+export function handleTransfer(event: Transfer): void {
+
+  
 }
 
-export function handleApprovalForAll(event: ApprovalForAll): void {}
+/**
+ * Tulips have been assigned to an owner.
+ * Need to create a ne tulip entity with claimed=false
+ * @param event 
+ */
+export function handlePrizePoolAwarded(event: PrizePoolAwarded): void {
+  // Check if exists
+  // let entity = TulipEntity.load(event.transaction.from.toHex())
 
-export function handleBaseURIChanged(event: BaseURIChanged): void {}
-
-export function handleContollerUpdated(event: ContollerUpdated): void {}
-
-export function handleOwnershipTransferred(event: OwnershipTransferred): void {}
-
-export function handleTransfer(event: Transfer): void {}
+  // if (!entity) {
+  //   entity = new TulipEntity(event.transaction.from.toHex())
+  // }
+  
+  log.debug("EVENT is: {}", [event.transaction.value.toString()]);
+}
